dbebb2bf5eca23bb597f0b91d193d4c5d693749e
Tuğrul Burak Gözdemir <tgrlgzdmr@gmail.com>
2022-04-22 16:42:05 +0300

    Add project files.

---
+++ Task/Controllers/DetailController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task.Models.Entity;
+
+namespace Task.Controllers
+{
+    public class DetailController : Controller
+    {
+        // GET: Detail
+        DbTaskEntities db = new DbTaskEntities();
+        public ActionResult Index(int id)
+        {
+            ViewBag.Id = id;
+            
+
+            var values = db.Tbl_Detail.Where(x => x.ItemId == id).ToList();
+            return View(values);
+            
+        }
+
+        [HttpGet]
+        public ActionResult AddDetail(int id)
+        {
+            ViewBag.b = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddDetail(Tbl_Detail p,int id)
+        {
+            p.Date = DateTime.Now;
+            p.ItemId = id;
+            db.Tbl_Detail.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index","Master");
+        }
+        
+        [HttpGet]
+        public ActionResult UpdateDetail(int id)
+        {
+            ViewBag.c = id;
+            Tbl_Detail t= db.Tbl_Detail.Find(id);
+            return View(t);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateDetail(Tbl_Detail p,int id)
+        {
+            
+            Tbl_Detail t = db.Tbl_Detail.Find(p.ItemId=id);
+            t.Date = DateTime.Now;
+            t.QTY = p.QTY;
+            
+            db.SaveChanges();
+            return RedirectToAction("Index","Master");
+        }
+
+        public ActionResult DeleteDetail(Tbl_Detail p,int id)
+        {
+            Tbl_Detail t =db.Tbl_Detail.Find(p.ItemId=id);
+            db.Tbl_Detail.Remove(t);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Master");
+
+
+        }
+    }
+}
---
+++ Task/Controllers/MasterController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task.Models.Entity;
+
+namespace Task.Controllers
+{
+    public class MasterController : Controller
+    {
+        
+        DbTaskEntities db = new DbTaskEntities();
+        public ActionResult Index()
+        {
+            var values = db.Tbl_Master.Where(x=>x.Situation==true).ToList();
+            return View(values); 
+        }
+
+        public ActionResult RecycleBin()
+        {
+            var values = db.Tbl_Master.Where(x => x.Situation == false).ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddItem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddItem(Tbl_Master p)
+        {
+            p.Situation = true;
+            db.Tbl_Master.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateItem(int id)
+        {
+            Tbl_Master t= db.Tbl_Master.Find(id);
+            return View(t);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateItem(Tbl_Master p)
+        {
+            Tbl_Master t=db.Tbl_Master.Find(p.Id);
+            t.Situation = true;
+            t.Name=p.Name;
+            t.Price = p.Price;
+            t.Unit=p.Unit;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteItem(Tbl_Master p)
+        {
+            Tbl_Master t = db.Tbl_Master.Find(p.Id);
+            t.Situation = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
